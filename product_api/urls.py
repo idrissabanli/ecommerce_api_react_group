3f992912563f7f6b2581cc31ee74030f4d21ee34
idris <idris.sabanli@gmail.com>
2020-06-05 01:25:16 +0400

    admin title

--- product_api/urls.py
+++ product_api/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.contrib import admin
 
 ...
 
@@ -48,3 +49,8 @@ urlpatterns = [
     re_path(r'^api-documentation/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# This is change default admin panel Headers and titles
+admin.site.site_header = 'E-commerce Admin'
+admin.site.site_title = 'E-commerce Administration'
+admin.site.index_title = 'E-commerce Administration'
