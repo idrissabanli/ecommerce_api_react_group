927dc895460252149ea8f094d4b7dc847a67b068
idris <idris.sabanli@gmail.com>
2021-03-04 01:16:28 +0400

    user

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import RegisterAPIView, LoginAPI
+from .views import RegisterAPIView, LoginAPI, ProfileAPIView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('api/login/', LoginAPI.as_view(), name='api_login'),
     path('api/register/', RegisterAPIView.as_view(), name='api_register'),
+    path('api/user-profile/', ProfileAPIView.as_view(), name='api_user_profile'),
 ]
