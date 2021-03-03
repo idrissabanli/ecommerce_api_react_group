7ba0112d33f890fccbf7d78d6180fc0c813a8d14
idris <idris.sabanli@gmail.com>
2021-03-04 01:16:25 +0400

    user

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 import django.contrib.auth.password_validation as validators
 from rest_framework.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
 
 User = get_user_model()
 
@@ -13,6 +14,10 @@ class UserProfileSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'first_name', 'last_name', 'image', 'token']
 
     def get_token(self, user):
+        request = self.context.get('request')
+        if request and hasattr(request, 'META') and request.META.get('HTTP_AUTHORIZATION'):
+            header_token = request.META.get('HTTP_AUTHORIZATION')
+            return header_token.split()[1]
         token, created = Token.objects.get_or_create(user=user)
         return token.key
 
@@ -28,4 +33,18 @@ class UserCreateSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+
+
+class ProfileUpdateSerializer(serializers.ModelSerializer):
+
+    def validate_email(self, value):
+        request = self.context.get('request')
+        user = request.user
+        if value != user.email:
+            raise serializers.ValidationError(_("You can not change your email"))
+        return value
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'image',]
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
 from django.contrib.auth import get_user_model
-from .serializers import UserCreateSerializer, UserProfileSerializer
+from .serializers import UserCreateSerializer, UserProfileSerializer, ProfileUpdateSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
+from rest_framework import permissions, status, generics
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from accounts.utils import CustomSwaggerAutoSchema
 
+User = get_user_model()
+
+
 class LoginAPI(ObtainAuthToken):
     custom_serializer_class = UserProfileSerializer
 
-    @swagger_auto_schema(auto_schema=CustomSwaggerAutoSchema, request_body=AuthTokenSerializer, responses={200: custom_serializer_class})
+    @swagger_auto_schema(auto_schema=CustomSwaggerAutoSchema, request_body=AuthTokenSerializer,
+                         responses={200: custom_serializer_class})
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
@@ -20,7 +24,33 @@ class LoginAPI(ObtainAuthToken):
         # user_serializer.is_valid(raise_exception=True)
         return Response(user_serializer.data)
 
-User = get_user_model()
+
+class ProfileAPIView(generics.GenericAPIView):
+    serializer_class = UserProfileSerializer
+    update_serializer_class = ProfileUpdateSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request):
+        return Response(data=self.serializer_class(request.user, context={'request': request}).data)
+
+    @swagger_auto_schema(request_body=update_serializer_class, responses={200: UserProfileSerializer})
+    def put(self, request):
+        data = request.data
+        serializer = self.update_serializer_class(request.user, data=data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        updated_user = serializer.save()
+        return Response(data=self.serializer_class(updated_user, context={'request': request}).data,
+                        status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(request_body=update_serializer_class, responses={200: UserProfileSerializer})
+    def patch(self, request):
+        data = request.data
+        serializer = self.update_serializer_class(request.user, data=data, partial=True, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        updated_user = serializer.save()
+        return Response(data=self.serializer_class(updated_user, context={'request': request}).data,
+                        status=status.HTTP_200_OK)
+
 
 class RegisterAPIView(CreateAPIView):
     model = User
